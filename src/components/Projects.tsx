07d5c4ec4a0e8714587dca41283293b5fcc62e24
Tobias Merkle <tobias@branchmessenger.com>
2024-07-06 13:10:03 -0400

    partners

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,20 +5,58 @@ import { isMobileCheck } from "../utils/dimensions";
 const PROJECTS = [
     {
         title: 'Barter',
-        description: 'Buy and sell NFTs by solving riddles and negotiating with a Telegram AI chatbot',
+        description: 'A tool for selling your NFTs through Telegram via a gamified AI assistant chat. Uses Kinode to integrate your wallets, your chats, and your LLM assistants.',
         image: '/barter.png'
     },
     {
         title: 'Filter',
-        description: 'Filter your Twitter feed with AI',
+        description: 'An LLM-powered Kinode app with associated browser extension that filters your Twitter feed based on customizeable rules.',
         image: '/filter.png'
     },
     {
         title: 'Command Center',
-        description: 'Configure AI chatbots and manage access keys for many different services',
+        description: 'A user-centric hub for controlling your Telegram chats and bots. Ingest and analyze data for future use and integrations.',
         image: '/command-center.png'
     }
 ]
+
+const PARTNERS = [
+    { 
+        title: 'MemeDeck',
+        description: 'The ultimate AI-powered meme generation platform. Users can create based, viral memes in seconds. No design skills required - you just need your imagination.',
+        image: '/memedeck.png',
+    },
+    {
+        title: 'Kinode',
+        description: 'Kinode OS combines the power of decentralized infrastructure with the flexibility of personal computing.',
+        image: '/kinode.png'
+    }
+]
+
+function ProjectPartnerCard({ project, index }: { project: { title: string, description: string, image: string }, index: number }) {
+    const isMobile = isMobileCheck()
+    return <div 
+        key={index} 
+        className={cn("flex bg-gray/10 rounded-md backdrop-blur-sm", {
+            'w-full gap-4 p-4': isMobile,
+            'w-[45%] gap-8 p-8 self-stretch': !isMobile
+        })}
+    >
+        <img src={project.image} alt={project.title} className={cn("rounded-md", {
+            'w-32 h-32': !isMobile,
+            'w-16 h-16': isMobile
+        })} />
+        <div className='flex flex-col gap-2'>
+            <h2 className={cn("font-bold", {
+                'text-2xl': !isMobile,
+                'text-xl': isMobile
+            })}>{project.title}</h2>
+            <p className={cn({
+                'text-lg': !isMobile
+            })}>{project.description}</p>
+        </div>
+    </div>
+}
 
 export default function Projects() {
     const isMobile = isMobileCheck()
@@ -37,27 +75,16 @@ export default function Projects() {
                 'gap-8': !isMobile
             })}>
                 {PROJECTS.map((project, index) => (
-                    <div 
-                        key={index} 
-                        className={cn("flex bg-gray/10 rounded-md backdrop-blur-sm", {
-                            'w-full gap-4 p-4': isMobile,
-                            'w-[45%] gap-8 p-8 self-stretch': !isMobile
-                        })}
-                    >
-                        <img src={project.image} alt={project.title} className={cn("self-center", {
-                            'w-32 h-32': !isMobile,
-                            'w-16 h-16': isMobile
-                        })} />
-                        <div className='flex flex-col'>
-                            <h2 className={cn("font-bold", {
-                                'text-2xl': !isMobile,
-                                'text-xl': isMobile
-                            })}>{project.title}</h2>
-                            <p className={cn({
-                                'text-lg': !isMobile
-                            })}>{project.description}</p>
-                        </div>
-                    </div>
+                    <ProjectPartnerCard project={project} index={index} />
+                ))}
+            </div>
+            <h2 className={cn("font-bold", {
+                'text-4xl': !isMobile,
+                'text-2xl': isMobile
+            })}>Partners</h2>
+            <div className="flex flex-wrap gap-8">
+                {PARTNERS.map((partner, index) => (
+                    <ProjectPartnerCard project={partner} index={index} />
                 ))}
             </div>
         </div>

6f122bdf5370c880f2171d311d4cd2a3130a6d43
Tobias Merkle <tobias@branchmessenger.com>
2024-12-03 19:44:58 -0500

    Revert "fe-tests"
    This reverts commit 9d5a75d8efe9fbeff6a672845bb0fe169306ad87.

--- src/tests/setup.ts
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom'
-
-// Use globalThis instead of global
-globalThis.ResizeObserver = jest.fn().mockImplementation(() => ({
-    observe: jest.fn(),
-    unobserve: jest.fn(),
-    disconnect: jest.fn(),
-}))
